0749d949104c67d5de1bdc2d57b694354a1b8717
DeveloperUchida <kouei28poiou@icloud.com>
2024-04-19 19:26:27 +0900

    DataUpdate 2024/04/19

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
 #include<iostream>
 #include<glad/gl.h>
 #include<GLFW/glfw3.h>
+
+int main()
+{
+    //GLFWの初期化
+    glfwInit();
+
+    //GLFWのバージョンを指定
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    
+}
