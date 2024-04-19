6e7162c7f177990ba2ce9677d588deffdb969c5a
DeveloperUchida <kouei28poiou@icloud.com>
2024-04-19 19:32:41 +0900

    DataUpdate 2024/04/19

--- main.cpp
+++ main.cpp
@@ -10,5 +10,15 @@ int main()
     //GLFWのバージョンを指定
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    //GLFWのプロファイルを指定
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    //Windowを作成
+    GLFWwindow* window = glfwCreateWindow(800,800, "OpenGL on MAC", NULL, NULL);
     
 }
