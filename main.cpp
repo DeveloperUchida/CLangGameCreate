9734852fe6e7c20c116a25d0b9beac1b89afdae3
DeveloperUchida <kouei28poiou@icloud.com>
2024-04-19 19:50:06 +0900

    DataUpdate 2024/04/19

--- main.cpp
+++ main.cpp
@@ -1,31 +1,35 @@
-#include<iostream>
-#include<glad/gl.h>
-#include<GLFW/glfw3.h>
+#include <iostream>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    //GLFWの初期化
+    // GLFWの初期化
     glfwInit();
 
-    //GLFWのバージョンを指定
+    // GLFWのバージョンを指定
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    //GLFWのプロファイルを指定
+    // GLFWのプロファイルを指定
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    //Windowを作成
-    GLFWwindow* window = glfwCreateWindow(800,800, "OpenGL on MAC", NULL, NULL);
-    if(window == NULL)
+    // Windowを作成
+    GLFWwindow *window = glfwCreateWindow(800, 800, "OpenGL on MAC", NULL, NULL);
+    if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    
+    // コンテキストの作成
+    glfwMakeContextCurrent(window);
+    // GLADをロードしてOpenGLの設定を行う
+    gladLoadGL(glfwGetProcAddress);
 
+    glViewport(0, 0, 800, 800);
 }
